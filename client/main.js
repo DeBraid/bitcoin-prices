94f4544745ec466953973a421327207b1cfeec68
DeBraid <de.braid@gmail.com>
2014-11-22 10:09:12 -0500

    prices loading to html

--- client/main.js
+++ client/main.js
@@ -4,6 +4,17 @@ Meteor.call("getPrice", function(error, result) {
   var price = result;
 
   console.log(price);
+  console.log("time", price[0].time);
   
-  // Session.set("currentPrice", price);
+  Session.set("currentPrice", price);
+  Session.set("time", parseInt(price[0].time));
 });
+
+Template.showPrices.helpers({
+  time: function () {
+    return Session.get("time");
+  }, 
+  priceData: function () {
+    return Session.get("currentPrice");
+  }
+}); 
