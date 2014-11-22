b11974279bf1be8466c308d0d2bfc1fd055c8e68
DeBraid <de.braid@gmail.com>
2014-11-22 15:29:34 -0500

    added data for Canada exchange

--- client/main.js
+++ client/main.js
@@ -11,11 +11,83 @@ Meteor.call("getPrice", function(error, result) {
     }
   ];
 
+
+
   return Session.set("currentPrice", data);
 });
+
+Meteor.call("getCAD24Price", function(error, result) {
+  if (error)
+      console.log(error)
+  var _this = result;
+  console.log("_this", _this);
+  console.log("_this.ticker_currency", _this.ticker_currency);
+
+  var data = [
+    {
+      time : _this.time_stamp,
+      coin : _this.coin_name,
+      currency : _this.ticker_currency,
+      price : _this.last_price
+    }
+  ];
+  return Session.set("CAD24", data);
+});
+
+
+Meteor.call("getBTCCADPrice", function(error, result) {
+  if (error)
+      console.log(error)
+  var getBTCCADPrice = result;
+  console.log("getBTCCADPrice", getBTCCADPrice);
+
+  // var data = [
+  //   {
+  //     time : price.time_stamp,
+  //     coin : price.ticker.coin_name,
+  //     price : price.ticker.bnc_price_index_usd
+  //   }
+  // ];
+  // return Session.set("currentPrice", data);
+});
+
+var ticks = [];
+
+Meteor.call("getPrice", function(error, result) {
+  if (error)
+      console.log(error)
+  var price = result;
+  var tick1 = price.time_stamp;
+  ticks.push(tick1);
+  var tick0 = ticks[ticks.length - 1];
+  console.log(ticks);
+  console.log(tick0);
+  console.log(tick1);
+  
+  // Meteor.setInterval(function() {
+  //     // update contents of slide data
+  //     tick += 1;
+  //     // set currentSlide Session object -- which triggers a redraw
+  //     Session.set('currentSlide', tick);
+  // }, 1000);
+
+  // if (tick0 != tick1 ) {
+  // }
+    Prices.insert(price);
+
+});
+
 
 Template.showPrices.helpers({
   priceData: function () {
     return Session.get("currentPrice");
   }
 }); 
+
+Template.canada.helpers({
+  CAD24: function () {
+    return Session.get("CAD24");
+  }
+}); 
+
+
--- server/server.js
+++ server/server.js
@@ -1,7 +1,20 @@
 Meteor.methods({
   getPrice: function () {
     var result = Meteor.http.call("GET", "http://api.bravenewcoin.com/ticker/bnc_ticker_btc.json");
-
     return result.data;
-   }
+  }  
+});
+
+Meteor.methods({
+  getCAD24Price: function () {
+    var result = Meteor.http.call("GET", "http://api.bravenewcoin.com/ticker/bnc_ticker_btc_cad_24hr.json");
+    return result.data;
+  }  
+});
+
+Meteor.methods({
+  getBTCCADPrice: function () {
+    var result = Meteor.http.call("GET", "http://api.bravenewcoin.com/ticker/bnc_ticker_btc_cad.json");
+    return result.data;
+  }  
 });
